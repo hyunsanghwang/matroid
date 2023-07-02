5a21bc010582c5385b007072c810f55834c8327c
hyunsanghwang <85355322+hyunsanghwang@users.noreply.github.com>
2023-07-02 16:32:33 +0900

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -5,6 +5,10 @@ package «matroid» {
   -- add any package configuration options here
 }
 
+lean_lib «DMvPolynomial» {
+  -- add library configuration options here
+}
+
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
